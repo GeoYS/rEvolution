8d30080db8ee9ba482e33905bb2b824826be1cbc
Christopher Stokes <Christopher Stokes@Chris-Yoga>
2014-05-29 20:04:41 -0400

    Properties Worked on

---
+++ rEvolution/src/revolution/game/creature/properties/PropertySet.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package revolution.game.creature.properties;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Christopher Stokes
+ */
+public class PropertySet {
+    HashMap<String, Property> properties = new HashMap<>();
+}
